fe930ef7e4b8354026f7fa77fd33fe75aa90b7eb
taylosstls <luisgustavogto@gmail.com>
2024-05-02 15:13:20 -0300

    Adjust Empyt Module

--- 01-challenge-concepts-todolist/src/components/ListToDo/EmptyModule.tsx
+++ 01-challenge-concepts-todolist/src/components/ListToDo/EmptyModule.tsx
@@ -1,9 +1,11 @@
 import styles from './Empty.module.css'
 
+import ClipboardImg from '@/assets/clipboard.png'
+
 export function EmptyModule() {
   return (
     <div className={styles.container}>
-      <img src="/clipboard.png" alt="ícone de prancheta" />
+      <img src={ClipboardImg} alt="Ícone de prancheta" />
       <p>
         <strong>Você ainda não tem tarefas cadastradas</strong>
         Crie tarefas e organize seus itens a fazer
