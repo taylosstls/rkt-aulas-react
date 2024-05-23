46e382ea0053f00a8202ac657c6e590e15b33005
taylosstls <luisgustavogto@gmail.com>
2024-05-23 15:22:49 -0300

    Add Implemention with Next Auth to Google Calendar

--- 06-nextjs-module/src/pages/register/connect-calendar/index.page.tsx
+++ 06-nextjs-module/src/pages/register/connect-calendar/index.page.tsx
@@ -1,34 +1,56 @@
+import { useState, useEffect } from 'react'
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
-import { ArrowRight } from 'phosphor-react'
-
+import { signIn, useSession } from 'next-auth/react'
+import { ArrowRight, CheckCircle } from 'phosphor-react'
 import { Container, Header } from '../styles'
 import { ConnectBox, ConnectItem } from './styles'
 
 export default function Register() {
-  // async function handleRegister(data: RegisterFormData) {}
+  const { data: session, status } = useSession()
+  const [isConnected, setIsConnected] = useState(false)
+
+  useEffect(() => {
+    if (session && status === 'authenticated') {
+      setIsConnected(true)
+    }
+  }, [session, status])
 
   return (
     <Container>
       <Header>
-        <Heading as={'h2'}>Conecte sua agenda!</Heading>
+        <Heading as="strong">Conecte sua agenda!</Heading>
         <Text>
           Conecte o seu calendário para verificar automaticamente as horas
           ocupadas e os novos eventos à medida em que são agendados.
         </Text>
 
-        <MultiStep size={4} currentStep={2}></MultiStep>
+        <MultiStep size={4} currentStep={2} />
       </Header>
 
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant={'secondary'} size={'sm'}>
-            Conectar
-            <ArrowRight />
+          <Button
+            variant={isConnected ? 'primary' : 'secondary'}
+            size="sm"
+            onClick={() => signIn('google')}
+            disabled={isConnected}
+          >
+            {isConnected ? (
+              <>
+                <CheckCircle />
+                Conectado
+              </>
+            ) : (
+              <>
+                Conectar
+                <ArrowRight />
+              </>
+            )}
           </Button>
         </ConnectItem>
 
-        <Button type="submit">
+        <Button type="submit" disabled={!isConnected}>
           Próximo passo
           <ArrowRight />
         </Button>
