ce8b497290377bac8780cfa18fe06c4031c6dcc3
taylosstls <luisgustavogto@gmail.com>
2024-05-23 11:01:04 -0300

    Add effect to load username value

--- 06-nextjs-module/src/pages/register/index.page.tsx
+++ 06-nextjs-module/src/pages/register/index.page.tsx
@@ -1,10 +1,13 @@
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 
-import { Container, Header, Form, FormError } from './styles'
 import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import { Container, Header, Form, FormError } from './styles'
 
 const registerFormSchema = z.object({
   username: z
@@ -23,10 +26,22 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
+    defaultValues: {
+      username: '',
+    },
   })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query?.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
     console.log(data)
