e817d3b32137f699ab74c9cd19afb7741bffe487
taylosstls <luisgustavogto@gmail.com>
2024-05-27 17:33:04 -0300

    Adjust Prisma Adapter

---
+++ 06-nextjs-module/src/@types/next-auth.d.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+
+//Sobreescrevendo algumas interfaces específicas do Typescript
+declare module 'next-auth' {
+  export interface User {
+    id: string;
+    name: string;
+    email: string;
+    username: string;
+    avatar_url: string | null;
+  }
+}
