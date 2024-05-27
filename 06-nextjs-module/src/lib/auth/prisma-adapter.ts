70a3bd9fb6c68d31666e7619bf38a5c4f36210df
taylosstls <luisgustavogto@gmail.com>
2024-05-27 18:02:41 -0300

    Adjust Prisma Adapter

--- 06-nextjs-module/src/lib/auth/prisma-adapter.ts
+++ 06-nextjs-module/src/lib/auth/prisma-adapter.ts
@@ -1,16 +1,51 @@
 import { Adapter } from "next-auth/adapters"
 import { prisma } from "../prisma"
+import { parseCookies, destroyCookie } from 'nookies'
+import { NextApiRequest, NextApiResponse } from "next"
 
-export function PrismaAdapter(): Adapter {
+export function PrismaAdapter(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Adapter {
   return {
-    // async createUser(user) {},
+    async createUser(user) {
+      const { '@ignitecall:userId': userIdOnCookies } = parseCookies({ req })
+
+      if (!userIdOnCookies) throw new Error('User ID not found on cookies.')
+
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: userIdOnCookies,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url,
+        },
+      })
+
+      destroyCookie({ res }, '@ignitecall:userId', {
+        path: '/',
+      })
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        email: prismaUser.email!,
+        emailVerified: null,
+        avatar_url: prismaUser.avatar_url,
+      }
+    },
 
     async getUser(id) {
-      const user = await prisma.user?.findUniqueOrThrow({
+      const user = await prisma.user?.findUnique({
         where: {
           id,
         }
       })
+
+      if (!user) return null
 
       return {
         id: user.id,
@@ -23,11 +58,13 @@ export function PrismaAdapter(): Adapter {
     },
 
     async getUserByEmail(email) {
-      const user = await prisma.user?.findUniqueOrThrow({
+      const user = await prisma.user?.findUnique({
         where: {
           email,
         }
       })
+
+      if (!user) return null
 
       return {
         id: user.id,
@@ -40,7 +77,7 @@ export function PrismaAdapter(): Adapter {
     },
 
     async getUserByAccount({ providerAccountId, provider }) {
-      const { user } = await prisma.account?.findUniqueOrThrow({
+      const account = await prisma.account?.findUnique({
         where: {
           provider_provider_account_id: {
             provider,
@@ -49,8 +86,12 @@ export function PrismaAdapter(): Adapter {
         },
         include: {
           user: true,
-        }
+        },
       })
+
+      if (!account) return null
+
+      const { user } = account
 
       return {
         id: user.id,
@@ -71,7 +112,7 @@ export function PrismaAdapter(): Adapter {
           name: user.name,
           email: user.email,
           avatar_url: user.avatar_url,
-        }
+        },
       })
 
       return {
@@ -119,14 +160,18 @@ export function PrismaAdapter(): Adapter {
     },
 
     async getSessionAndUser(sessionToken) {
-      const { user, ...session } = await prisma.session.findUniqueOrThrow({
+      const prismaSession = await prisma.session.findUnique({
         where: {
           session_token: sessionToken,
         },
         include: {
           user: true,
-        }
+        },
       })
+
+      if (!prismaSession) return null
+
+      const { user, ...session } = prismaSession
 
       return {
         session: {
@@ -141,7 +186,7 @@ export function PrismaAdapter(): Adapter {
           email: user.email!,
           emailVerified: null,
           avatar_url: user.avatar_url,
-        }
+        },
       }
     },
 
@@ -161,6 +206,14 @@ export function PrismaAdapter(): Adapter {
         userId: prismaSession.user_id,
         expires: prismaSession.expires,
       }
+    },
+
+    async deleteSession(sessionToken) {
+      await prisma.session.delete({
+        where: {
+          session_token: sessionToken
+        }
+      })
     }
 
   }
--- 06-nextjs-module/src/pages/api/auth/[...nextauth].api.ts
+++ 06-nextjs-module/src/pages/api/auth/[...nextauth].api.ts
@@ -1,31 +1,41 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '../../../lib/auth/prisma-adapter'
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          scope:
-            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
-        },
-      },
-    }),
-  ],
+export function buildNextAuthOptions(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
 
-  callbacks: {
-    async signIn({ account }) {
-      return (
-        '/register/connect-calendar' +
-        (!account?.scope?.includes('https://www.googleapis.com/auth/calendar')
-          ? '?error=permissions'
-          : '')
-      )
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        authorization: {
+          params: {
+            scope:
+              'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+          },
+        },
+      }),
+    ],
+
+    callbacks: {
+      async signIn({ account }) {
+        return (
+          '/register/connect-calendar' +
+          (!account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+            ? '?error=permissions'
+            : '')
+        )
+      },
     },
-  },
+  }
 }
-export default NextAuth(authOptions)
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildNextAuthOptions(req, res))
+}
