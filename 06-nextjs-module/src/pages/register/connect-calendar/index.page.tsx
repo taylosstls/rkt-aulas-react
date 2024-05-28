b281b1f4012a486fb07cfc9d2d7f9bd2a5c54733
taylosstls <luisgustavogto@gmail.com>
2024-05-28 11:29:02 -0300

    Creating time-intervals page and business rule

--- 06-nextjs-module/src/pages/register/connect-calendar/index.page.tsx
+++ 06-nextjs-module/src/pages/register/connect-calendar/index.page.tsx
@@ -2,11 +2,11 @@ import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import { ArrowRight, CheckCircle, WarningCircle } from 'phosphor-react'
-// import { api } from "../../../lib/axios"
+
 import { Container, Header } from '../styles'
 import { AuthError, ConnectBox, ConnectItem } from './styles'
 
-export default function Register() {
+export default function ConnectCalendar() {
   const session = useSession()
   const router = useRouter()
 
