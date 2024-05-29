245d6f7bdaeeef8657c62d05b6de13990d26b3ce
taylosstls <luisgustavogto@gmail.com>
2024-05-29 17:59:03 -0300

    Create Grid Picker time List

--- 06-nextjs-module/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ 06-nextjs-module/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,34 @@
 import { Calendar } from '../../../../../components/Calendar'
-import { Container } from './styles'
+import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from './styles'
 
 export default function CalendarStep() {
+  const DateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={DateSelected}>
       <Calendar />
+
+      {DateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20 de maio</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>10:00h</TimePickerItem>
+            <TimePickerItem>11:00h</TimePickerItem>
+            <TimePickerItem>12:00h</TimePickerItem>
+            <TimePickerItem>13:00h</TimePickerItem>
+            <TimePickerItem>14:00h</TimePickerItem>
+            <TimePickerItem>15:00h</TimePickerItem>
+            <TimePickerItem>16:00h</TimePickerItem>
+            <TimePickerItem>17:00h</TimePickerItem>
+            <TimePickerItem>18:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
