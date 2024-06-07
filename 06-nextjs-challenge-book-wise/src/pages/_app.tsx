d6871063c0b599b5bfc45ff12a3e53b6cb8ccc4d
taylosstls <luisgustavogto@gmail.com>
2024-06-07 12:15:10 -0300

    Create Book Wise initial structure project

---
+++ 06-nextjs-challenge-book-wise/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app'
+import { Nunito } from 'next/font/google'
+
+import { globalStyles } from '@/styles/globals'
+
+export const nunito = Nunito({ subsets: ['latin'] })
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={nunito.className}>
+      <Component {...pageProps} />
+    </div>
+  )
+}
